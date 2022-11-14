1b43402fe129e821031473bd74569945388543c9
karla castillo <kacastillo1@me.com>
2022-11-13 21:34:31 -0600

    Added 3 conditions to message

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -15,16 +15,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         if messageLabel.text == "You Are Awesome!" {
             messageLabel.text = "You Are Great!"
             imageView.image = UIImage (named: "image1")
-        } else {
-            messageLabel.text = "You Are Awesome!"
-            imageView.image = UIImage (named: "image0")
+        } else if messageLabel.text == "You Are Great!" {
+            messageLabel.text = "You Are Da Bomb!"
+            imageView.image = UIImage (named: "image2")
+        }  else {
+                messageLabel.text = "You Are Awesome!"
+                imageView.image = UIImage (named: "image0")
+            }
         }
-    }
-    
 }
-
+ 
